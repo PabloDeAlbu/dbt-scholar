9dd5916f2c0e535c3e9e70dd51f13cea1eb01831
Pablo de Albuquerque <pablo.cda@gmail.com>
2025-05-27 08:07:52 -0300

    Agrego fact publication de openalex

--- models/openalex/04_dm/fact_publication_openalex.sql
+++ models/openalex/04_dm/fact_publication_openalex.sql
@@ -1,6 +1,5 @@
 WITH base as (
     SELECT
-        hub_work.work_hk,
         hub_work.work_id,
         hub_doi.doi,
         hub_mag.mag,
@@ -36,7 +35,12 @@ WITH base as (
         sat_work.apc_paid_currency,
         sat_work.apc_paid_provenance,
         sat_work.apc_paid_value,
-        sat_work.apc_paid_value_usd
+        sat_work.apc_paid_value_usd,
+        hub_work.work_hk,
+        hub_doi.doi_hk,
+        hub_mag.mag_hk,
+        hub_pmcid.pmcid_hk,
+        hub_pmid.pmid_hk
     FROM {{ref('hub_openalex_work')}} hub_work
     INNER JOIN {{ref('sat_openalex_work')}} sat_work ON sat_work.work_hk = hub_work.work_hk
     LEFT JOIN {{ref('link_openalex_work_doi')}} link_work_doi ON link_work_doi.work_hk = hub_work.work_hk
@@ -47,6 +51,52 @@ WITH base as (
     LEFT JOIN {{ref('hub_openalex_pmcid')}} hub_pmcid ON hub_pmcid.pmcid_hk = link_work_pmcid.pmcid_hk
     LEFT JOIN {{ref('link_openalex_work_pmid')}} link_work_pmid ON link_work_pmid.work_hk = hub_work.work_hk
     LEFT JOIN {{ref('hub_openalex_pmid')}} hub_pmid ON hub_pmid.pmid_hk = link_work_pmid.pmid_hk
+),
+
+fillna as (
+    SELECT 
+        work_id,
+        COALESCE(doi, 'NO DATA') as doi,
+        COALESCE(mag, 'NO DATA') as mag,
+        COALESCE(pmcid, 'NO DATA') as pmcid,
+        COALESCE(pmid, 'NO DATA') as pmid,
+        COALESCE(title, 'NO DATA') as title,
+        COALESCE(type, 'NO DATA') as type,
+        COALESCE(publication_year, 0) as publication_year,
+        publication_date,
+        COALESCE(countries_distinct_count, 0) as countries_distinct_count,
+        COALESCE(institutions_distinct_count, 0) as institutions_distinct_count,
+        COALESCE(fwci, 0) as fwci,
+        COALESCE(has_fulltext, False) as has_fulltext,
+        COALESCE(fulltext_origin, 'NO DATA') as fulltext_origin,
+        COALESCE(cited_by_count, 0) as cited_by_count,
+        COALESCE(is_retracted, False) as is_retracted,
+        COALESCE(is_paratext, False) as is_paratext,
+        COALESCE(locations_count, 0) as locations_count,
+        COALESCE(referenced_works_count, 0) as referenced_works_count,
+        COALESCE(any_repository_has_fulltext, False) as any_repository_has_fulltext,
+        COALESCE(is_oa, False) as is_oa,
+        COALESCE(oa_status, 'NO DATA') as oa_status,
+        COALESCE(oa_url, 'NO DATA') as oa_url,
+        COALESCE(cited_by_percentile_year_max, 0) as cited_by_percentile_year_max,
+        COALESCE(cited_by_percentile_year_min, 0) as cited_by_percentile_year_min,
+        COALESCE(citation_normalized_percentile_is_in_top_10_percent, False) as citation_normalized_percentile_is_in_top_10_percent,
+        COALESCE(citation_normalized_percentile_is_in_top_1_percent, False) as citation_normalized_percentile_is_in_top_1_percent,
+        COALESCE(citation_normalized_percentile_value, 0) as citation_normalized_percentile_value,
+        COALESCE(apc_list_currency, 'NO DATA') as apc_list_currency,
+        COALESCE(apc_list_provenance, 'NO DATA') as apc_list_provenance,
+        COALESCE(apc_list_value, 0) as apc_list_value,
+        COALESCE(apc_list_value_usd, 0) as apc_list_value_usd,
+        COALESCE(apc_paid_currency, 'NO DATA') as apc_paid_currency,
+        COALESCE(apc_paid_provenance, 'NO DATA') as apc_paid_provenance,
+        COALESCE(apc_paid_value, 0) as apc_paid_value,
+        COALESCE(apc_paid_value_usd, 0) as apc_paid_value_usd,
+        work_hk,
+        doi_hk,
+        mag_hk,
+        pmcid_hk,
+        pmid_hk
+    FROM base
 )
 
-SELECT * FROM base
+SELECT * FROM fillna
