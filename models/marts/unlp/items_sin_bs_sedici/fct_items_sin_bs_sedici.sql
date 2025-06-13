33d8e81eefe835c74df9164ee4a1c71386805ce0
Pablo de Albuquerque <pablo.cda@gmail.com>
2025-06-13 13:41:16 -0300

    en marts agrego fct_item_sin_bs_sedici para unlp

--- models/marts/unlp/items_sin_bs_sedici/fct_items_sin_bs_sedici.sql
+++ models/marts/unlp/items_sin_bs_sedici/fct_items_sin_bs_sedici.sql
@@ -2,26 +2,76 @@
 
 WITH base as (
 	SELECT 
-		h.text_value as item_handle, 
-		t.text_value as item_title, 
-		origin.text_value as origin, 
+		handle.text_value as item_handle,
+		type.text_value as type,
+		subtype.text_value as subtype,
+		title.text_value as item_title,
+		origin.text_value as origin,
 		(uri.text_value IS NOT NULL) as has_sedici_identifier_uri,
-		c.title as col_title, 
-		c.handle as col_handle
+		(loc.text_value IS NOT NULL) as has_mods_location,
+		c.handle as col_handle,
+		c.title as col_title,
+		mv_com.text_value as com_title
 	FROM {{ref('fct_item_dspace5')}} i
-	INNER JOIN {{ref('dim_item_metadatavalue_dspace5')}} h ON 
-		i.item_hk = h.item_hk AND h.short_id = 'dc' AND h.element = 'identifier' AND h.qualifier = 'uri'
-	INNER JOIN {{ref('dim_item_metadatavalue_dspace5')}} t ON 
-		i.item_hk = t.item_hk AND t.short_id = 'dc' AND t.element = 'title' AND t.qualifier is null
-	LEFT JOIN {{ref('bridge_item_bitstream_dspace5')}} i_bs ON
-		i.item_hk = i_bs.item_hk
-	LEFT JOIN {{ref('dim_item_metadatavalue_dspace5')}} origin ON
-		i.item_hk = origin.item_hk AND origin.short_id = 'mods' AND origin.element = 'originInfo' AND origin.qualifier = 'place'
-	LEFT JOIN {{ref('dim_item_metadatavalue_dspace5')}} uri ON
-		i.item_hk = uri.item_hk AND uri.short_id = 'sedici' AND uri.element = 'identifier' AND uri.qualifier = 'uri'
+
+	INNER JOIN {{ref('bridge_item_metadatavalue_dspace5')}} bridge_dc_identifier_uri ON 
+		bridge_dc_identifier_uri.item_hk = i.item_hk AND 
+		bridge_dc_identifier_uri.metadatafield_fullname = 'dc.identifier.uri'
+	INNER JOIN {{ref('dim_metadatavalue_dspace5')}} handle ON 
+		handle.metadatavalue_hk = bridge_dc_identifier_uri.metadatavalue_hk
+
+	INNER JOIN {{ref('bridge_item_metadatavalue_dspace5')}} bridge_dc_title ON 
+		bridge_dc_title.item_hk = i.item_hk AND 
+		bridge_dc_title.metadatafield_fullname = 'dc.title'
+	INNER JOIN {{ref('dim_metadatavalue_dspace5')}} title ON 
+		title.metadatavalue_hk = bridge_dc_title.metadatavalue_hk
+
+	INNER JOIN {{ref('bridge_item_metadatavalue_dspace5')}} bridge_dc_type ON 
+		bridge_dc_type.item_hk = i.item_hk AND 
+		bridge_dc_type.metadatafield_fullname = 'dc.type'
+	INNER JOIN {{ref('dim_metadatavalue_dspace5')}} type ON 
+		type.metadatavalue_hk = bridge_dc_type.metadatavalue_hk
+
+	INNER JOIN {{ref('bridge_item_metadatavalue_dspace5')}} bridge_sedici_subtype ON 
+		bridge_sedici_subtype.item_hk = i.item_hk AND 
+		bridge_sedici_subtype.metadatafield_fullname = 'sedici.subtype'
+	INNER JOIN {{ref('dim_metadatavalue_dspace5')}} subtype ON 
+		subtype.metadatavalue_hk = bridge_sedici_subtype.metadatavalue_hk
+
+	LEFT JOIN {{ref('bridge_item_metadatavalue_dspace5')}} bridge_mods_origininfo_place ON 
+		bridge_mods_origininfo_place.item_hk = i.item_hk AND 
+		bridge_mods_origininfo_place.metadatafield_fullname = 'mods.originInfo.place'
+	LEFT JOIN {{ref('dim_metadatavalue_dspace5')}} origin ON 
+		origin.metadatavalue_hk = bridge_mods_origininfo_place.metadatavalue_hk
+
+	LEFT JOIN {{ref('bridge_item_metadatavalue_dspace5')}} bridge_sedici_identifier_uri ON 
+		bridge_sedici_identifier_uri.item_hk = i.item_hk AND 
+		bridge_sedici_identifier_uri.metadatafield_fullname = 'sedici.identifier.uri'
+	LEFT JOIN {{ref('dim_metadatavalue_dspace5')}} uri ON 
+		uri.metadatavalue_hk = bridge_dc_title.metadatavalue_hk
+
+	LEFT JOIN {{ref('bridge_item_metadatavalue_dspace5')}} bridge_mods_location ON 
+		bridge_mods_location.item_hk = i.item_hk AND 
+		bridge_mods_location.metadatafield_fullname = 'mods.location'
+	LEFT JOIN {{ref('dim_metadatavalue_dspace5')}} loc ON 
+		loc.metadatavalue_hk = bridge_dc_title.metadatavalue_hk
+
 	INNER JOIN {{ref('dim_collection_dspace5')}} c ON
 		i.owningcollection_hk = c.collection_hk
-	WHERE i_bs.item_hk IS NULL AND h.text_value like 'http://sedici.unlp.edu.ar/handle/10915/%'
+
+	LEFT JOIN {{ref('bridge_item_bitstream_dspace5')}} i_bs ON
+		i.item_hk = i_bs.item_hk
+
+	INNER JOIN {{ ref('link_dspace5_community_collection')}} lnk_com_col ON
+		lnk_com_col.collection_hk = c.collection_hk
+
+	INNER JOIN {{ ref('bridge_community_metadatavalue_dspace5') }} brd_com_mv ON 
+		brd_com_mv.community_hk = lnk_com_col.community_hk AND brd_com_mv.metadatafield_fullname = 'dc.title'
+
+	INNER JOIN {{ ref('dim_metadatavalue_dspace5')}} mv_com ON
+		brd_com_mv.metadatavalue_hk = mv_com.metadatavalue_hk
+
+	WHERE i_bs.item_hk IS NULL AND handle.text_value like 'http://sedici.unlp.edu.ar/handle/10915/%'
 )
 
 SELECT * FROM base
