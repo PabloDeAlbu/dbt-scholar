0bbdae07ce8127c26d460e486ab1de70f06fcc54
Pablo de Albuquerque <pablo.cda@gmail.com>
2025-08-25 11:00:07 -0300

    update marts/unlp

---
+++ models/marts/03_org/unlp/dim_unlp_subject.sql
@@ -0,0 +1,14 @@
+WITH subject AS (
+    SELECT
+        mv.text_value::varchar as text_value,
+        COUNT(*)::int as count
+    FROM {{ref('brg_dspace5_item_metadatavalue')}} bridge_i_mv
+    INNER JOIN {{ref('dim_dspace5_metadatavalue')}} mv ON 
+        mv.metadatavalue_hk = bridge_i_mv.metadatavalue_hk
+    WHERE
+        bridge_i_mv.metadatafield_fullname = 'dc.subject'
+    GROUP BY mv.text_value
+    ORDER BY count(*) DESC
+)
+
+SELECT * FROM subject
