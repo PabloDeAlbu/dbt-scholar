c2ee4d374046059bc8e5833d91b2ce51cbb30030
Pablo de Albuquerque <pablo.cda@gmail.com>
2025-10-23 18:39:58 -0300

    Ajusto viz openalex author

--- models/marts/05_viz/01_base/openalex/viz_openalex_author.sql
+++ models/marts/05_viz/01_base/openalex/viz_openalex_author.sql
@@ -1,6 +1,11 @@
 WITH base AS (
     SELECT 
-        *
+        REPLACE(author_id, 'https://openalex.org/', '') as author_id,
+        display_name,
+        works_count,
+        cited_by_count,
+        orcid,
+        has_orcid
     FROM {{ref('dim_openalex_author')}}
 )
 
--- models/marts/05_viz/01_base/openalex/viz_openalex_work_author.sql
+++ models/marts/05_viz/01_base/openalex/viz_openalex_work_author.sql
@@ -7,6 +7,9 @@ WITH base AS (
         REPLACE(dim_a.author_id, 'https://openalex.org/', '') as author_id,
         dim_a.display_name,
         COALESCE(orcid, '-') as orcid,
+        has_orcid,
+        works_count,
+        cited_by_count,
         brg.work_hk,
         brg.author_hk
     FROM {{ref('brg_openalex_work_author')}} brg
