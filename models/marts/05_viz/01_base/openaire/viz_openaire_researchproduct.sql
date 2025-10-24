edea6f46e9b3882f83e2c72c9568f2b8cabe248e
Pablo de Albuquerque <pablo.cda@gmail.com>
2025-10-24 08:11:26 -0300

    Agrego type en viz openaire researchproduct instancetype

--- models/marts/05_viz/01_base/openaire/viz_openaire_researchproduct.sql
+++ models/marts/05_viz/01_base/openaire/viz_openaire_researchproduct.sql
@@ -1,5 +1,35 @@
 WITH base AS (
-    SELECT * 
+    SELECT 
+        researchproduct_id,
+
+        publication_date,
+        embargo_end_date,
+        type,
+        main_title,
+        language_code,
+        language_label,
+        publisher,
+
+        best_access_right,
+        best_access_right_uri,
+        publicly_funded,
+        is_green,
+        is_in_diamond_journal,
+        
+        downloads,
+        views,
+        
+        citation_class,
+        citation_count,
+        impulse,
+        impulse_class,
+        influence,
+        influence_class,
+        popularity,
+        popularity_class
+
+        has_publication_date
+
     FROM {{ref('fct_openaire_researchproduct_publication')}}
 )
 
--- models/marts/05_viz/01_base/openaire/viz_openaire_researchproduct_instancetype.sql
+++ models/marts/05_viz/01_base/openaire/viz_openaire_researchproduct_instancetype.sql
@@ -1,10 +1,12 @@
 WITH base AS (
     SELECT 
-        researchproduct_id,
-        instance_type as type
-    FROM {{ ref('hub_openaire_researchproduct') }}
+        hub.researchproduct_id,
+        fct.type,
+        dim_instance_type.instance_type
+    FROM {{ ref('hub_openaire_researchproduct') }} hub
+    INNER JOIN {{ ref('fct_openaire_researchproduct_publication') }} fct USING (researchproduct_hk)
     LEFT JOIN {{ ref('brg_openaire_researchproduct_instances') }} USING (researchproduct_hk)
-    LEFT JOIN {{ ref('dim_openaire_instance_type') }} USING (instancetype_hk)
+    LEFT JOIN {{ ref('dim_openaire_instance_type') }} dim_instance_type USING (instancetype_hk)
 )
 
 SELECT * FROM base
