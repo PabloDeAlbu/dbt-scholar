f7d4dcac72474e9c20dd8bd464f71ffce28f68b9
Pablo de Albuquerque <pablo.cda@gmail.com>
2025-10-24 13:50:31 -0300

    Agrego a los viz de topic de openalex los domain, field y subfield

--- models/marts/05_viz/01_base/openalex/viz_openalex_work_primarytopic.sql
+++ models/marts/05_viz/01_base/openalex/viz_openalex_work_primarytopic.sql
@@ -6,6 +6,12 @@ WITH base AS (
         REPLACE(dim_w.work_id, 'https://openalex.org/', '') as work_id,
         REPLACE(dim_t.topic_id, 'https://openalex.org/', '') as topic_id,
         dim_t.display_name,
+        dim_t.domain_display_name,
+        dim_t.domain_id,
+        dim_t.field_display_name,
+        dim_t.field_id,
+        dim_t.subfield_display_name,
+        dim_t.subfield_id,
         brg.primarytopic_score,
         brg.topic_hk,
         brg.work_hk
--- models/marts/05_viz/01_base/openalex/viz_openalex_work_topic.sql
+++ models/marts/05_viz/01_base/openalex/viz_openalex_work_topic.sql
@@ -7,6 +7,12 @@ WITH base AS (
         REPLACE(dim_t.topic_id, 'https://openalex.org/', '') as topic_id,
         dim_t.display_name,
         brg.topic_hk,
+        dim_t.domain_display_name,
+        dim_t.domain_id,
+        dim_t.field_display_name,
+        dim_t.field_id,
+        dim_t.subfield_display_name,
+        dim_t.subfield_id,
         brg.work_hk
     FROM {{ref('brg_openalex_work_topic')}} brg
     INNER JOIN {{ref('dim_openalex_work')}} dim_w USING (work_hk)
