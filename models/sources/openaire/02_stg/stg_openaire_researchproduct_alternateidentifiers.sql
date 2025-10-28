4ee9933adf02025c634c84b95b1506a7ffbcb9c6
Pablo de Albuquerque <pablo.cda@gmail.com>
2025-10-28 13:48:30 -0300

    agrego stg_openaire_researchproduct_alternateidentifiers

---
+++ models/sources/openaire/02_stg/stg_openaire_researchproduct_alternateidentifiers.sql
@@ -0,0 +1,31 @@
+{{ config(materialized='view') }}
+
+{%- set yaml_metadata -%}
+source_model: "ldg_openaire_researchproduct_alternateidentifiers"
+derived_columns:
+  source: "!OPENAIRE"
+  load_datetime: load_datetime
+hashed_columns:
+  researchproduct_hk: researchproduct_id
+  pid_hk:    
+    - scheme
+    - value
+  researchproduct_pid_hk:
+    - researchproduct_id
+    - scheme
+    - value
+  researchproduct_alternateidentifier_hk:
+    is_hashdiff: true
+    columns:
+      - scheme
+      - value 
+{%- endset -%}
+
+{% set metadata_dict = fromyaml(yaml_metadata) %}
+
+{{ automate_dv.stage(include_source_columns=true,
+                     source_model=metadata_dict['source_model'],
+                     derived_columns=metadata_dict['derived_columns'],
+                     null_columns=none,
+                     hashed_columns=metadata_dict['hashed_columns'],
+                     ranked_columns=none) }}
