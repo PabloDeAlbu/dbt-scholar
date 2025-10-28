9409075aea85170ffcedcc5ea45da87f4df4a6dc
Pablo de Albuquerque <pablo.cda@gmail.com>
2025-10-28 11:30:26 -0300

    Agrego flag para ver si los research product tienen algun pid en viz openaire researchproduct

--- models/marts/05_viz/01_base/openaire/viz_openaire_researchproduct.sql
+++ models/marts/05_viz/01_base/openaire/viz_openaire_researchproduct.sql
@@ -1,6 +1,6 @@
 WITH base AS (
     SELECT 
-        researchproduct_id,
+        fct.researchproduct_id,
 
         publication_date,
         embargo_end_date,
@@ -26,11 +26,47 @@ WITH base AS (
         influence,
         influence_class,
         popularity,
-        popularity_class
+        popularity_class,
 
-        has_publication_date
+        has_publication_date,
 
-    FROM {{ref('fct_openaire_researchproduct_publication')}}
+        has_arxiv,
+        has_single_arxiv,
+        arxiv_count,
+
+        has_doi,
+        has_single_doi,
+        doi_count,
+
+        has_handle,
+        has_single_handle,
+        handle_count,
+
+        has_mag,
+        has_single_mag,
+        mag_count,
+
+        has_pmb,
+        has_single_pmb,
+        pmb_count,
+
+        has_pmc,
+        has_single_pmc,
+        pmc_count,
+
+        has_pmid,
+        has_single_pmid,
+        pmid_count
+
+    FROM {{ref('fct_openaire_researchproduct_publication')}} fct
+    LEFT JOIN {{ref('viz_openaire_researchproduct_pid_doi')}} USING (researchproduct_hk)
+    LEFT JOIN {{ref('viz_openaire_researchproduct_pid_handle')}} USING (researchproduct_hk)
+    LEFT JOIN {{ref('viz_openaire_researchproduct_pid_arxiv')}} USING (researchproduct_hk)
+    LEFT JOIN {{ref('viz_openaire_researchproduct_pid_mag')}} USING (researchproduct_hk)
+    LEFT JOIN {{ref('viz_openaire_researchproduct_pid_pmb')}} USING (researchproduct_hk)
+    LEFT JOIN {{ref('viz_openaire_researchproduct_pid_pmc')}} USING (researchproduct_hk)
+    LEFT JOIN {{ref('viz_openaire_researchproduct_pid_pmid')}} USING (researchproduct_hk)
 )
 
 SELECT * FROM base
+-- WHERE has_arxiv = false AND has_doi = false AND has_handle = false AND has_mag = false AND has_pmb = false AND has_pmc = false AND has_pmid = false
