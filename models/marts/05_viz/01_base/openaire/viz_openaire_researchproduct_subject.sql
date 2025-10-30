0a6628a361b897a6137d0de18a969219c27334f5
Pablo de Albuquerque <pablo.cda@gmail.com>
2025-10-30 10:21:03 -0300

    add viz openaire research product subject

---
+++ models/marts/05_viz/01_base/openaire/viz_openaire_researchproduct_subject.sql
@@ -0,0 +1,11 @@
+WITH base AS (
+    SELECT 
+        researchproduct_id,
+        dim_openaire_subject.subject_scheme,
+        dim_openaire_subject.subject_value
+    FROM {{ ref('hub_openaire_researchproduct') }}
+    LEFT JOIN {{ ref('brg_openaire_researchproduct_subject') }} USING (researchproduct_hk)
+    LEFT JOIN {{ ref('dim_openaire_subject') }} USING (subject_hk)
+)
+
+SELECT * FROM base
