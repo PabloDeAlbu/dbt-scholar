bb059f43fb5f1cebaf0b02ce9aceee7474156e9c
Pablo de Albuquerque <pablo.cda@gmail.com>
2025-11-25 12:53:19 -0300

    fix typo

--- models/sources/oai/04_dm/dim_oai_col.sql
+++ models/sources/oai/04_dm/dim_oai_col.sql
@@ -2,7 +2,7 @@ WITH base AS (
     SELECT 
         dim_set.set_id,
         dim_set.name,
-        CASE WHEN COALESCE(hub_col.col_id, '-') <> '-' THEN TRUE ELSE FALSE END AS proceseed,
+        CASE WHEN COALESCE(hub_col.col_id, '-') <> '-' THEN TRUE ELSE FALSE END AS processed,
         dim_set.set_hk
     FROM {{ ref('dim_oai_set') }} dim_set
     LEFT JOIN {{ ref('hub_oai_col')}} hub_col ON hub_col.col_hk = dim_set.set_hk
