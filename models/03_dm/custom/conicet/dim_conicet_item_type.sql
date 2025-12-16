b241be7d6acc79e6b89c2bf72ba7a55ce57b254a
Pablo de Albuquerque <pablo.cda@gmail.com>
2025-12-16 11:14:46 -0300

    fix seed de coar con resource type para dim conicet item type

--- models/03_dm/custom/conicet/dim_conicet_item_type.sql
+++ models/03_dm/custom/conicet/dim_conicet_item_type.sql
@@ -1,12 +1,12 @@
 WITH base AS (
     SELECT 
-        hub_type.dc_type,
+        UPPER(REPLACE(hub_type.dc_type, 'info:ar-repo/semantics/','')) as dc_type,
         seed.label,
         seed.coar_uri,
         hub_type.dc_type_hk
     FROM {{ ref('hub_oai_type') }} hub_type
     INNER JOIN {{ref('seed_coar_resource_types2conicet_oai_dc_types')}} seed ON hub_type.dc_type = seed.record_type
-    WHERE coar_uri != '#N/A' and dc_type like 'info:%'
+    WHERE coar_uri != '#N/A' and dc_type like 'info:ar-repo%'
     ORDER BY 1
 )
 
