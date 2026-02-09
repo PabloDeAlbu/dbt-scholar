816d2f7ee68ccf2587360f22baca2290f933edcd
Pablo de Albuquerque <pablo.cda@gmail.com>
2026-02-09 11:10:04 -0300

    Agrego col valid_date_issued para fct_oai_record_publication

--- models/03_dm/core/oai/fct_oai_record_publication.sql
+++ models/03_dm/core/oai/fct_oai_record_publication.sql
@@ -7,8 +7,12 @@ base as (
         sat_i.record_id,
         sat_i.title,
         sat_i.date_issued,
-        sat_i.record_hk
-        
+        sat_i.record_hk,
+        coalesce(
+            sat_i.date_issued >= DATE '1900-01-01'
+            AND sat_i.date_issued < date_trunc('year', current_date) + interval '1 year',
+            false
+        ) AS valid_date_issued
     FROM latest_sat sat_i
 )
 
