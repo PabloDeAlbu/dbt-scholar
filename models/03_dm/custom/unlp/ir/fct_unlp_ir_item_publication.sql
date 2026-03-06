0fb0c0e3ea79ae421786448e91c98a5bbe52f711
Pablo de Albuquerque <pablo.cda@gmail.com>
2026-03-06 13:15:00 -0300

    dm(unlp): add IR item fact model + schema tests

--- models/03_dm/custom/unlp/ir/fct_unlp_ir_item_publication.sql
+++ models/03_dm/custom/unlp/ir/fct_unlp_ir_item_publication.sql
@@ -3,20 +3,28 @@
 WITH id AS (
     SELECT
         i_mv.item_hk,
-        mv.text_value
-    FROM {{ref('er_dspace5_item_metadatavalue')}} i_mv
-    JOIN {{ref('er_dspace5_metadatavalue')}} mv USING (metadatavalue_hk)
+        MIN(mv.text_value) AS dc_identifier_uri,
+        STRING_AGG(mv.text_value, '|' ORDER BY mv.text_value) AS dc_identifier_uri_raw
+    FROM {{ ref('brg_dspace5_item_metadatavalue') }} i_mv
+    JOIN {{ ref('dim_dspace5_metadatavalue') }} mv USING (metadatavalue_hk)
     WHERE
         i_mv.metadatafield_fullname = 'dc.identifier.uri' AND
         mv.text_value LIKE 'http://sedici.unlp.edu.ar/handle/10915%'
+    GROUP BY i_mv.item_hk
 ),
 
 date_accessioned AS (
   SELECT 
     i_mv.item_hk,
-    STRING_AGG(mv.text_value, '|' ORDER BY mv.text_value) as text_value
-  FROM {{ref('er_dspace5_item_metadatavalue')}} i_mv
-  JOIN {{ref('er_dspace5_metadatavalue')}} mv USING (metadatavalue_hk)
+    MIN(mv.text_value) FILTER (
+        WHERE mv.text_value ~ '^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$'
+    ) AS iso_date,
+    MIN(mv.text_value) FILTER (
+        WHERE mv.text_value ~ '^[0-9]{4}$'
+    ) AS year_only,
+    STRING_AGG(mv.text_value, '|' ORDER BY mv.text_value) AS raw_values
+  FROM {{ ref('brg_dspace5_item_metadatavalue') }} i_mv
+  JOIN {{ ref('dim_dspace5_metadatavalue') }} mv USING (metadatavalue_hk)
   WHERE i_mv.metadatafield_fullname = 'dc.date.accessioned'
   GROUP BY i_mv.item_hk
 ),
@@ -24,9 +32,15 @@ date_accessioned AS (
 date_issued AS (
   SELECT 
     i_mv.item_hk,
-    STRING_AGG(mv.text_value, '|' ORDER BY mv.text_value) as text_value
-  FROM {{ref('er_dspace5_item_metadatavalue')}} i_mv
-  JOIN {{ref('er_dspace5_metadatavalue')}} mv USING (metadatavalue_hk)
+    MIN(mv.text_value) FILTER (
+        WHERE mv.text_value ~ '^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$'
+    ) AS iso_date,
+    MIN(mv.text_value) FILTER (
+        WHERE mv.text_value ~ '^[0-9]{4}$'
+    ) AS year_only,
+    STRING_AGG(mv.text_value, '|' ORDER BY mv.text_value) AS raw_values
+  FROM {{ ref('brg_dspace5_item_metadatavalue') }} i_mv
+  JOIN {{ ref('dim_dspace5_metadatavalue') }} mv USING (metadatavalue_hk)
   WHERE i_mv.metadatafield_fullname = 'dc.date.issued'
   GROUP BY i_mv.item_hk
 ),
@@ -35,17 +49,30 @@ dc_type AS (
     SELECT
         i_mv.item_hk,
         mv.text_value
-    FROM {{ref('er_dspace5_item_metadatavalue')}} i_mv
-    JOIN {{ref('er_dspace5_metadatavalue')}} mv USING (metadatavalue_hk)
+    FROM {{ ref('brg_dspace5_item_metadatavalue') }} i_mv
+    JOIN {{ ref('dim_dspace5_metadatavalue') }} mv USING (metadatavalue_hk)
     WHERE i_mv.metadatafield_fullname = 'dc.type'
 ),
 
 final as (
     SELECT 
+        item.item_hk,
         item.item_id,
-        id.text_value as id,
-        to_timestamp(date_accessioned.text_value,'YYYY-MM-DD') as date_accessioned,
-        to_timestamp(date_accessioned.text_value,'YYYY-MM-DD') as date_issued,
+        id.dc_identifier_uri,
+        id.dc_identifier_uri_raw,
+
+        date_accessioned.raw_values AS date_accessioned_raw,
+        CASE
+            WHEN date_accessioned.iso_date IS NOT NULL THEN TO_DATE(date_accessioned.iso_date, 'YYYY-MM-DD')
+            WHEN date_accessioned.year_only IS NOT NULL THEN TO_DATE(date_accessioned.year_only || '-01-01', 'YYYY-MM-DD')
+        END AS date_accessioned,
+
+        date_issued.raw_values AS date_issued_raw,
+        CASE
+            WHEN date_issued.iso_date IS NOT NULL THEN TO_DATE(date_issued.iso_date, 'YYYY-MM-DD')
+            WHEN date_issued.year_only IS NOT NULL THEN TO_DATE(date_issued.year_only || '-01-01', 'YYYY-MM-DD')
+        END AS date_issued,
+
         dc_type.text_value as type,
         in_archive,
         withdrawn,
@@ -53,8 +80,8 @@ final as (
     FROM {{ref('fct_dspace5_item')}} item
     JOIN id USING (item_hk)
     JOIN dc_type USING (item_hk)
-    JOIN date_accessioned USING (item_hk)
-    JOIN date_issued USING (item_hk)
+    LEFT JOIN date_accessioned USING (item_hk)
+    LEFT JOIN date_issued USING (item_hk)
 )
 
 SELECT * FROM final
