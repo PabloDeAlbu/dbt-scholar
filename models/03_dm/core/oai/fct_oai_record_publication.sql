110f6b08251e62360ea8cd751fcf48e7cd7d68f1
Pablo de Albuquerque <pablo.cda@gmail.com>
2026-04-04 17:10:04 -0300

    refactor(dm core) normaliza oai y centraliza dimensiones coar

--- models/03_dm/core/oai/fct_oai_record_publication.sql
+++ models/03_dm/core/oai/fct_oai_record_publication.sql
@@ -10,8 +10,42 @@ latest_extract AS (
     FROM {{ ref('latest_sat_oai_record__extract') }}
 ),
 
+latest_extract_by_record AS (
+    SELECT
+        record_hk,
+        repository_identifier,
+        institution_ror,
+        extract_datetime,
+        load_datetime
+    FROM (
+        SELECT
+            *,
+            ROW_NUMBER() OVER (
+                PARTITION BY record_hk
+                ORDER BY
+                    extract_datetime DESC NULLS LAST,
+                    load_datetime DESC NULLS LAST,
+                    repository_identifier,
+                    institution_ror
+            ) AS extract_rank
+        FROM latest_extract
+    ) ranked
+    WHERE extract_rank = 1
+),
+
+type_agg AS (
+    SELECT
+        brg.record_hk,
+        STRING_AGG(DISTINCT hub_t.dc_type, '|' ORDER BY hub_t.dc_type) AS dc_type,
+        COUNT(DISTINCT hub_t.dc_type_hk) AS dc_type_count,
+        (COUNT(DISTINCT hub_t.dc_type_hk) > 1) AS has_multiple_dc_type
+    FROM {{ ref('brg_oai_record_type') }} brg
+    INNER JOIN {{ ref('hub_oai_type') }} hub_t USING (dc_type_hk)
+    GROUP BY brg.record_hk
+),
+
 base as (
-    SELECT DISTINCT
+    SELECT
         sat_r.record_id,
         sat_r.title,
         sat_r.date_issued,
@@ -20,20 +54,21 @@ base as (
         extract.institution_ror,
         extract.extract_datetime,
         extract.load_datetime,
-        hub_t.dc_type,
+        type_agg.dc_type,
+        COALESCE(type_agg.dc_type_count, 0) AS dc_type_count,
+        COALESCE(type_agg.has_multiple_dc_type, false) AS has_multiple_dc_type,
         coalesce(
             sat_r.date_issued >= DATE '1900-01-01'
             AND sat_r.date_issued < date_trunc('year', current_date) + interval '1 year',
             false
         ) AS valid_date_issued,
         coalesce(
-            hub_t.dc_type_hk IS NOT NULL,
+            type_agg.dc_type_count > 0,
             false
         ) AS valid_dc_type
     FROM latest_sat sat_r
-    LEFT JOIN latest_extract extract USING (record_hk)
-    LEFT JOIN {{ref('brg_oai_record_type')}} USING (record_hk)
-    LEFT JOIN {{ref('hub_oai_type')}} hub_t USING (dc_type_hk)
+    LEFT JOIN latest_extract_by_record extract USING (record_hk)
+    LEFT JOIN type_agg USING (record_hk)
     WHERE sat_r._context = 'request'
 )
 
