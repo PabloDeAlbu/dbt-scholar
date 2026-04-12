44a194d0e511a8ddfcae125f1eb3b2dbf9a3ea24
Pablo de Albuquerque <pablo.cda@gmail.com>
2026-04-12 19:02:32 -0300

    refactor(dm core openaire) enriquece bridge de originalid

--- models/03_dm/core/openaire/brg_openaire_researchproduct_originalid.sql
+++ models/03_dm/core/openaire/brg_openaire_researchproduct_originalid.sql
@@ -1,11 +1,14 @@
 {{ config(materialized = 'table') }}
 
-WITH base as (
-    SELECT 
-        researchproduct_hk,
-        original_hk,
-        researchproduct_originalid_hk
-    FROM {{ref('link_openaire_researchproduct_originalid')}} 
+WITH base AS (
+    SELECT
+        link.researchproduct_hk,
+        link.original_hk,
+        link.researchproduct_originalid_hk,
+        stg.original_id
+    FROM {{ ref('link_openaire_researchproduct_originalid') }} AS link
+    INNER JOIN {{ ref('stg_openaire_researchproduct_originalid') }} AS stg
+        USING (researchproduct_originalid_hk)
 )
 
 SELECT * FROM base
