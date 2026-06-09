578fd641f9fc6cc01260f6f1f8d2491f19d77e25
Pablo de Albuquerque <pablo.cda@gmail.com>
2026-06-09 11:25:16 -0300

    chore(viz unlp): retira datasets derivados sin uso

--- models/04_viz/custom/unlp/sedici_scholar_coverage/int_unlp_scholar_audit_recent_items.sql
+++
@@ -1,8 +0,0 @@
-{{ config(materialized='view') }}
-
-SELECT
-    'recent_items'::text AS audit_scenario,
-    'date_accessioned desc'::text AS cohort_rule,
-    base.*
-FROM {{ ref('base_unlp_scholar_audit_candidate') }} AS base
-WHERE base.date_accessioned IS NOT NULL
