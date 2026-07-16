393a9d8195d117be94183468741ef9c65cca5641
Pablo de Albuquerque <pablo.cda@gmail.com>
2026-07-16 13:34:32 -0300

    fix(conicet): valida identificadores del dataset OAI

--- models/04_org/conicet/oai/fct_conicet_oai_record_pid_profile.sql
+++ models/04_org/conicet/oai/fct_conicet_oai_record_pid_profile.sql
@@ -25,13 +25,13 @@ classified AS (
         dc_identifier_uri,
         CASE
             WHEN dc_identifier_uri IS NULL THEN 'null'
-            WHEN dc_identifier_uri ~ '^http://hdl\\.handle\\.net/11336/' THEN 'http_handle_11336'
-            WHEN dc_identifier_uri ~ '^https://hdl\\.handle\\.net/11336/' THEN 'https_handle_11336'
+            WHEN dc_identifier_uri ~ '^http://hdl\.handle\.net/11336/' THEN 'http_handle_11336'
+            WHEN dc_identifier_uri ~ '^https://hdl\.handle\.net/11336/' THEN 'https_handle_11336'
             WHEN dc_identifier_uri ~ '11336' THEN 'contains_11336_other'
             ELSE 'other'
         END AS dc_identifier_uri_pattern,
         COALESCE(
-            dc_identifier_uri ~ '^https?://hdl\\.handle\\.net/11336/',
+            dc_identifier_uri ~ '^https?://hdl\.handle\.net/11336/',
             false
         ) AS dc_identifier_uri_is_valid,
         dc_relation_doi,
